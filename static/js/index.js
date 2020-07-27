0ecf304d49ffed80e3bbee589279d33cc0a26a78
Pratik Anand <grufelous@gmail.com>
2020-07-28 02:49:44 +0530

    WiFi detection added

--- static/js/index.js
+++ static/js/index.js
@@ -2,6 +2,8 @@ const fs = require('fs');
 const path = require('path');
 const {ipcRenderer} = require('electron')
 
+var wifi = require("node-wifi");
+
 // List of all files in the data directory
 let fileNames = [];
 let fileNameForm = document.getElementById('fileNameForm');
@@ -9,6 +11,12 @@ let titlesList = document.getElementById('titles');
 let editSpace = document.getElementById('contentDisplay');
 let fileSaveBtn = document.getElementById('fileSaveBtn');
 let fileTitleLabel = document.getElementById('fileTitle');
+
+// Initialize wifi module
+// Absolutely necessary even to set interface to null
+wifi.init({
+    iface: null // network interface, choose a random wifi interface if set to null
+});
 
 fileSaveBtn.addEventListener('click', function() {
     let fileName = fileTitleLabel.innerText + '.md';
@@ -90,3 +98,79 @@ fileNameForm.addEventListener('submit', function(e) {
     }
     fileNameForm.style.display = 'none';
 })
+
+function scanNetworks() {
+    // Scan networks
+    wifi.scan(function(err, networks) {
+        if (err) {
+        console.log(err);
+        } else {
+        console.log(networks);
+        /*
+            networks = [
+                {
+                    ssid: '...',
+                    bssid: '...',
+                    mac: '...', // equals to bssid (for retrocompatibility)
+                    channel: <number>,
+                    frequency: <number>, // in MHz
+                    signal_level: <number>, // in dB
+                    quality: <number>, // same as signal level but in %
+                    security: 'WPA WPA2' // format depending on locale for open networks in Windows
+                    security_flags: '...' // encryption protocols (format currently depending of the OS)
+                    mode: '...' // network mode like Infra (format currently depending of the OS)
+                },
+                ...
+            ];
+            */
+        }
+    });
+}
+
+function connectNetwork() {
+    // Connect to a network
+    wifi.connect({ ssid: "ssid", password: "password" }, function(err) {
+        if (err) {
+            console.log(err);
+        }
+        console.log("Connected");
+    });
+  
+}
+
+function currentNetwork() {
+    // List the current wifi connections
+    wifi.getCurrentConnections(function(err, currentConnections) {
+        if (err) {
+            console.log(err);
+        }
+        console.log(currentConnections);
+        /*
+        // you may have several connections
+        [
+            {
+                iface: '...', // network interface used for the connection, not available on macOS
+                ssid: '...',
+                bssid: '...',
+                mac: '...', // equals to bssid (for retrocompatibility)
+                channel: <number>,
+                frequency: <number>, // in MHz
+                signal_level: <number>, // in dB
+                quality: <number>, // same as signal level but in %
+                security: '...' //
+                security_flags: '...' // encryption protocols (format currently depending of the OS)
+                mode: '...' // network mode like Infra (format currently depending of the OS)
+            }
+        ]
+        */
+    });
+}
+// All functions also return promise if there is no callback given
+/*wifi
+  .scan()
+  .then(function(networks) {
+    // networks
+  })
+  .catch(function(error) {
+    // error
+  });*/
